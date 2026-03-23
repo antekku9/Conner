2931dad89025d91cceb7b112f9ccc38734fac840
antekku9 <antekku9@gmail.com>
2026-03-23 16:54:55 +0100

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,10 @@
 import { RouterProvider } from 'react-router';
 import { router } from './routes';
 
+if (router && !router.basename) {
+  router.basename = "/Conner";
+}
+
 export default function App() {
   return <RouterProvider router={router} />;
 }
