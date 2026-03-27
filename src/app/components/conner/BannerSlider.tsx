96ca3d362c8e5e5ff75b64ac400a7fee1daf96c7
antekku9 <antekku9@gmail.com>
2026-03-27 12:01:46 +0100

    Update banner image URL in BannerSlider component

--- src/app/components/conner/BannerSlider.tsx
+++ src/app/components/conner/BannerSlider.tsx
@@ -9,7 +9,7 @@ import { ImageWithFallback } from '../figma/ImageWithFallback';
 // Każdy baner może zawierać: image (URL obrazu), title, subtitle, ctaText, ctaLink
 const banners = [
   {
-    image: 'https://unsplash.com/photos/hand-installing-a-computer-processor-onto-a-motherboard-DQBHliTwIaU,
+    image: 'https://images.unsplash.com/photo-1756801370266-f589801cedc3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D,
     title: 'Profesjonalny Serwis IT',
     subtitle: 'Kompleksowa obsługa komputerów, laptopów i serwerów',
     ctaText: 'Dowiedz się więcej',
